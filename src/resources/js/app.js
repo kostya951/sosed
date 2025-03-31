461e20907b71dfc68d577b1029aa0e64943e76d4
Kostya Bronshteyn <mrkostya951@mail.ru>
2025-03-31 18:14:32 +0300

    added images and bootstrap to main js module

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,1 +1,17 @@
-import './bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import '../css/app.css';
+import '../css/my.css';
+import './images'
+import {createApp} from 'vue/dist/vue.esm-bundler';
+import TestComponent from './components/TestComponent.vue';
+
+import axios from 'axios';
+window.axios = axios;
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+const app = createApp({});
+app.component('test-component',TestComponent);
+app.mount('#app');
+
---
+++ src/resources/js/images.js
@@ -0,0 +1,6 @@
+import '../img/logo.png';
+import '../img/favicon.png';
+import '../img/main.png';
+import '../img/nophoto.png';
+import 'bootstrap-icons/icons/bell.svg';
+import 'bootstrap-icons/icons/person-circle.svg';
