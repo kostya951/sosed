996065bedd36e7edf61afd840ab3a3a9db63444b
Kostya Bronshteyn <mrkostya951@mail.ru>
2025-04-02 14:23:24 +0300

    registered vue components

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -4,7 +4,8 @@ import '../css/app.css';
 import '../css/my.css';
 import './images'
 import {createApp} from 'vue/dist/vue.esm-bundler';
-import TestComponent from './components/TestComponent.vue';
+import SignupForm from './components/SignupForm.vue';
+import LoginForm from './components/LoginForm.vue';
 
 import axios from 'axios';
 window.axios = axios;
@@ -12,6 +13,7 @@ window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 const app = createApp({});
-app.component('test-component',TestComponent);
+app.component('signup-form',SignupForm);
+app.component('login-form',LoginForm);
 app.mount('#app');
 
